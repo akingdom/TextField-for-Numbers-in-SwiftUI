07d48a89ff5be49619e1bb418248623f2879c086
akingdom <1809762+akingdom@users.noreply.github.com>
2021-04-13 11:00:41 +1000

    Decimals and non-English numbers

--- TextField for Numbers in SwiftUI/TextField for Numbers in SwiftUI/NumberTextField.swift
+++ TextField for Numbers in SwiftUI/TextField for Numbers in SwiftUI/NumberTextField.swift
@@ -3,7 +3,7 @@
 //  TextField for Numbers in SwiftUI
 //
 //  Created by Can Balkaya on 1/5/21.
-//
+//  Modified by Andrew Kingdom to allow decimals -- needs more testing
 
 import SwiftUI
 
@@ -22,7 +22,7 @@ struct NumberTextField<V>: UIViewRepresentable where V: Numeric & LosslessString
     func makeUIView(context: UIViewRepresentableContext<NumberTextField>) -> UITextField {
         let editField = UITextField()
         editField.delegate = context.coordinator
-        editField.keyboardType = .numberPad
+        editField.keyboardType = .decimalPad
 
         return editField
     }
@@ -35,7 +35,9 @@ struct NumberTextField<V>: UIViewRepresentable where V: Numeric & LosslessString
 
         // MARK: - Properties
         var value: Binding<V>
-
+        var isEditing = false
+        var newValue: String? = ""
+        
         // MARK: - Life Cycle
         init(value: Binding<V>) {
             self.value = value
@@ -44,24 +46,39 @@ struct NumberTextField<V>: UIViewRepresentable where V: Numeric & LosslessString
         // MARK: - Functions
         func textField(_ textField: UITextField, shouldChangeCharactersIn range: NSRange, replacementString string: String) -> Bool {
             let text = textField.text as NSString?
-            let newValue = text?.replacingCharacters(in: range, with: string)
-
-            if let number = V(newValue ?? "0") {
-                self.value.wrappedValue = number
-
+            self.newValue = text?.replacingCharacters(in: range, with: string)
+            let formatter: NumberFormatter = NumberFormatter()
+            formatter.locale = NSLocale(localeIdentifier: "EN") as Locale?
+            let final = formatter.number(from: (newValue ?? "" ) as String)
+            if newValue!.isEmpty {
                 return true
-            } else {
-                if nil == newValue || newValue!.isEmpty {
-                    self.value.wrappedValue = 0
-                }
-
-                return false
             }
+            else if final != nil {
+                return true
+            } else if nil == newValue || newValue!.isEmpty {
+                self.value.wrappedValue = 0  // default
+            }
+            return false
         }
 
+        func textFieldDidBeginEditing(_ textField: UITextField) {
+            isEditing = true
+        }
+
+        // Needed??
+        func textFieldShouldEndEditing(_ textField: UITextField) -> Bool {
+            if (self.newValue ?? "").isEmpty {
+                self.value.wrappedValue = 0  // default
+            }
+            return true
+        }
         func textFieldDidEndEditing(_ textField: UITextField, reason: UITextField.DidEndEditingReason) {
             if reason == .committed {
                 textField.resignFirstResponder()
+                isEditing = false
+                if (self.newValue ?? "").isEmpty {
+                    self.value.wrappedValue = 0  // default
+                }
             }
         }
     }
